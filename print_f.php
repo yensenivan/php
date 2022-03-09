a15b5d6ad529d93a29ef122f32a9ee6b362c7fb0
Yensen Ivan <90391230+yensenivan@users.noreply.github.com>
2022-03-09 09:15:55 -0300

    php

---
+++ print_f.php
@@ -0,0 +1,22 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+function print_f($variable){
+    $contenido = "";
+    if(is_array($variable)){
+foreach($variable as $item);{
+    $contenido .= $item . "\n";
+}
+ file_put_contents("datos.txt", $contenido);
+     } else {
+         file_put_contents("datos.txt", $variable);
+     }
+}
+
+$aNotas = array(8,5,7,9,10);
+$msg = "Este es un mensaje";
+print_f($aNotas);
+echo "Archivo generado";
+?>
